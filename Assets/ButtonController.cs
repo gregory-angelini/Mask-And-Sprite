0d688c91c69e6e5b5d1036fe5c131fe065595d5b
gregory_vovchok <gregory.vovchok@gmail.com>
2020-11-10 08:24:49 -0800

    added button click sound

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        audioSource.Play();
+    }
+}
